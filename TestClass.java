a2a3122b361305b8c89b8f9b3a541cea5f2b34ee
unknown <sheumbbaria@gmail.com>
2019-09-18 00:03:08 +0600

    Create TestClass.java

---
+++ TestClass.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class TestClass {
+	
+		public String name,gender;
+		public int phone;
+		public void displayinf() {
+			System.out.println(name);
+			System.out.println(gender);
+			System.out.println(phone);
+		}
+}
